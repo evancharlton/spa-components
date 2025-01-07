4d574dcbcd150174da17158942f3aa52f5ffe746
Evan Charlton <evancharlton@gmail.com>
2025-01-07 06:23:10 +0100

    fix: Use replace on the language selection
    Choosing a language shouldn't add to the history back stack -- the apps
    should rely on this being changed via their settings dialogs.

--- LanguageSelector/LanguageSelector.tsx
+++ LanguageSelector/LanguageSelector.tsx
@@ -4,8 +4,12 @@ import classes from "./LanguageSelector.module.css";
 export const LanguageSelector = () => {
   return (
     <div className={classes.container}>
-      <Link to="/nb">bokmål</Link>
-      <Link to="/nn">nynorsk</Link>
+      <Link replace to="/nb">
+        bokmål
+      </Link>
+      <Link replace to="/nn">
+        nynorsk
+      </Link>
     </div>
   );
 };
